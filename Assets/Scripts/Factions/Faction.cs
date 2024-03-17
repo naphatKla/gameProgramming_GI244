1f372400d1feee9a611f1e1d51289bc3a78b55a4
Kla <naphat.watt@bumail.net>
2024-03-17 22:49:44 +0700

    Worker Added

--- Assets/Scripts/Factions/Faction.cs
+++ Assets/Scripts/Factions/Faction.cs
@@ -43,6 +43,10 @@ public class Faction : MonoBehaviour
     [SerializeField] private Transform ghostBuildingParent;
     public Transform GhostBuildingParent { get { return ghostBuildingParent; } }
     
+    [SerializeField]
+    private Transform startPosition; //start position for Faction
+    public Transform StartPosition { get { return startPosition; } }
+    
     
     void Start()
     {
@@ -113,5 +117,37 @@ public class Faction : MonoBehaviour
         wood -= building.StructureCost.wood;
         gold -= building.StructureCost.gold;
         stone -= building.StructureCost.stone;
+    }
+    
+    public Vector3 GetHQSpawnPos()
+    {
+        foreach (Building b in aliveBuildings)
+        {
+            if (b.IsHQ)
+                return b.SpawnPoint.position;
+        }
+        return startPosition.position;
+    }
+    
+    public void GainResource(ResourceType resourceType, int amount)
+    {
+        switch (resourceType)
+        {
+            case ResourceType.Food:
+                food += amount;
+                break;
+            case ResourceType.Wood:
+                wood += amount;
+                break;
+            case ResourceType.Gold:
+                gold += amount;
+                break;
+            case ResourceType.Stone:
+                stone += amount;
+                break;
+        }
+
+        if (this == GameManager.instance.MyFaction)
+            MainUI.instance.UpdateAllResource(this);
     }
 }
--- Assets/Scripts/Units/Worker.cs
+++ Assets/Scripts/Units/Worker.cs
@@ -43,6 +43,15 @@ public class Worker : MonoBehaviour
             case UnitState.MoveToResource:
                 MoveToResourceUpdate();
                 break;
+            case UnitState.Gather:
+                GatherUpdate();
+                break;
+            case UnitState.DeliverToHQ:
+                DeliverToHQUpdate();
+                break;
+            case UnitState.StoreAtHQ:
+                StoreAtHQUpdate();
+                break;
         }
     }
     
@@ -76,4 +85,54 @@ public class Worker : MonoBehaviour
             }
         }
     }
+    
+    private void GatherUpdate()
+    {
+        if (Time.time - lastGatherTime > gatherRate)
+        {
+            lastGatherTime = Time.time;
+
+            if (amountCarry < maxCarry)
+            {
+                if (curResourceSource != null)
+                {
+                    curResourceSource.GatherResource(gatherAmount);
+
+                    carryType = curResourceSource.RsrcType;
+                    amountCarry += gatherAmount;
+                }
+            }
+            else //amount is full, go back to deliver at HQ
+                unit.SetState(UnitState.DeliverToHQ);
+        }
+    }
+    
+    private void DeliverToHQUpdate()
+    {
+        if (Time.time - unit.LastPathUpdateTime > unit.PathUpdateRate)
+        {
+            unit.LastPathUpdateTime = Time.time;
+
+            unit.NavAgent.SetDestination(unit.Faction.GetHQSpawnPos());
+            unit.NavAgent.isStopped = false;
+        }
+
+        if (Vector3.Distance(transform.position, unit.Faction.GetHQSpawnPos()) <= 1f)
+            unit.SetState(UnitState.StoreAtHQ);
+    }
+    
+    private void StoreAtHQUpdate()
+    {
+        unit.LookAt(unit.Faction.GetHQSpawnPos());
+
+        if (amountCarry > 0)
+        {
+            // Deliver the resource to Faction
+            unit.Faction.GainResource(carryType, amountCarry);
+            amountCarry = 0;
+
+            //Debug.Log("Delivered");
+        }
+    }
+
 }
